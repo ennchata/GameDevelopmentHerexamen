9c3cab5c051561db8806bd79a72d57f5648d6ae5
s150673 <thibo.maes@student.ap.be>
2025-06-18 21:56:18 +0200

    Add input state helper class

---
+++ GameDevelopmentHerexamen/Framework/Utility/InputState.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Framework.Utility {
+    public class InputState {
+        public KeyboardState KeyboardState { get; private set; }
+        public MouseState MouseState { get; private set; }
+
+        public void Update() {
+            KeyboardState = Keyboard.GetState();
+            MouseState = Mouse.GetState();
+        }
+
+        public bool IsKeyDown(Keys key) => KeyboardState.IsKeyDown(key);
+        public bool IsLeftClickPressed() => MouseState.LeftButton == ButtonState.Pressed;
+    }
+}
