f7e49caa63da86907ff1d2ec0f4a9538cb918339
s150673 <thibo.maes@student.ap.be>
2025-06-18 21:44:30 +0200

    Add universal dimension helper classes

---
+++ GameDevelopmentHerexamen/Framework/Utility/UDim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Framework.Utility {
+    public class UDim(float scale, float offset) {
+        public float Scale = scale;
+        public float Offset = offset;
+
+        public float Resolve(float parentSize) {
+            return parentSize * Scale + Offset;
+        }
+    }
+}
---
+++ GameDevelopmentHerexamen/Framework/Utility/UDim2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Framework.Utility {
+    public class UDim2 {
+        public UDim X;
+        public UDim Y;
+
+        public UDim2(UDim x, UDim y) {
+            X = x;
+            Y = y;
+        }
+
+        public UDim2(float xScale, float xOffset, float yScale, float yOffset) {
+            X = new UDim(xScale, xOffset);
+            Y = new UDim(yScale, yOffset);
+        }
+
+        public Vector2 Resolve(Vector2 parentSize) {
+            return new Vector2(X.Resolve(parentSize.X), Y.Resolve(parentSize.Y));
+        }
+    }
+}
