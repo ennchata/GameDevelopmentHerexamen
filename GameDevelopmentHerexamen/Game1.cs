b627a63afd4df727ec494d94f13240e58b8e7df5
s150673 <thibo.maes@student.ap.be>
2025-06-20 17:40:01 +0200

    Update game class methods

--- GameDevelopmentHerexamen/Game1.cs
+++ GameDevelopmentHerexamen/Game1.cs
@@ -1,50 +1,43 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Scene;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace GameDevelopmentHerexamen
-{
-    public class Game1 : Game
-    {
+namespace GameDevelopmentHerexamen {
+    public class Game1 : Game {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
-        {
+        public Game1() {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        protected override void Initialize() {
+            GameObject.InitializeDebug(_graphics.GraphicsDevice);
+            SceneManager.Instance.GraphicsDevice = _graphics.GraphicsDevice;
 
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {
+        protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+        protected override void Update(GameTime gameTime) {
+            SceneManager.Instance.Update(gameTime);
 
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            SceneManager.Instance.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
