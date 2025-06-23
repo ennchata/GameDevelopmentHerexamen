e9944904effeb6d36f0e3b0bda4787df73cdd44d
s150673 <thibo.maes@student.ap.be>
2025-06-24 00:08:23 +0200

    Add key handler object for global shorcut -> event

---
+++ GameDevelopmentHerexamen/Implementation/Object/Input/KeyHandlerObject.cs
@@ -0,0 +1,56 @@
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Object.Component;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Implementation.Object.Input {
+    public class KeyHandlerObject : GameObject {
+        public Keys Key { get; set; }
+
+        public delegate void StateChangedEventHandler(Keys key, bool isDown);
+        public event StateChangedEventHandler StateChanged;
+
+        private bool previousState = false;
+
+        public KeyHandlerObject(Keys key,
+                Action<Keys, bool>? anyHandler = null,
+                Action<Keys>? keyDownHandler = null,
+                Action<Keys>? keyUpHandler = null) {
+            Key = key;
+
+            if (anyHandler != null) {
+                StateChanged += anyHandler.Invoke;
+            }
+
+            if (keyDownHandler != null) {
+                StateChanged += (key, isDown) => {
+                    if (isDown) {
+                        keyDownHandler.Invoke(key);
+                    }
+                };
+            }
+
+            if (keyUpHandler != null) {
+                StateChanged += (key, isDown) => {
+                    if (!isDown) {
+                        keyUpHandler.Invoke(key);
+                    }
+                };
+            }
+
+            AddComponent(new InputComponent((owner, inputState) => {
+                KeyHandlerObject handlerOwner = owner as KeyHandlerObject;
+                bool state = inputState.IsKeyDown(handlerOwner.Key);
+               
+                if (handlerOwner.previousState != state) {
+                    handlerOwner.previousState = state;
+                    handlerOwner.StateChanged.Invoke(handlerOwner.Key, state);
+                }
+            }));
+        }
+    }
+}
--- GameDevelopmentHerexamen/Implementation/Scene/Menu/SplashScreenScene.cs
+++ GameDevelopmentHerexamen/Implementation/Scene/Menu/SplashScreenScene.cs
@@ -3,6 +3,7 @@ using GameDevelopmentHerexamen.Framework.Object.Component;
 using GameDevelopmentHerexamen.Framework.Scene;
 using GameDevelopmentHerexamen.Framework.Utility;
 using GameDevelopmentHerexamen.Implementation.Component;
+using GameDevelopmentHerexamen.Implementation.Object.Input;
 using GameDevelopmentHerexamen.Implementation.Object.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -16,15 +17,9 @@ using System.Threading.Tasks;
 namespace GameDevelopmentHerexamen.Implementation.Scene.Menu {
     public class SplashScreenScene : GameScene {
         public SplashScreenScene() : base([
-            new GameObject() {
-                Components = [
-                    new InputComponent((self, inputState) => {
-                        if (inputState.IsKeyDown(Keys.Escape)) {
-                            Game1.ShouldExit();
-                        }
-                    })
-                ]
-            },
+            new KeyHandlerObject(Keys.Escape, keyDownHandler: (key) => {
+                Game1.ShouldExit();
+            }),
             new TextObject("fonts/roboto36", "My First Platformer", textPosition: new UDim2(0.5f, 0, 0.25f, 0)),
             new TextObject("fonts/roboto14", "Game Development Herexamen - Thibo Maes", textAnchor: new Vector2(0f, 1f), textPosition: UDim2.BottomLeft + (10, -35)),
             new TextObject("fonts/roboto14", "Press [Esc] to close game", textAnchor: new Vector2(0f, 1f), textPosition: UDim2.BottomLeft + (10, -10))
