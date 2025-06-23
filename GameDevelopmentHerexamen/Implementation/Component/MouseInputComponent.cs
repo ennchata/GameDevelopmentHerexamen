71bec082ed8809a231aae1e1463a9a19df9b1acb
s150673 <thibo.maes@student.ap.be>
2025-06-24 00:54:22 +0200

    Add mouse input component

---
+++ GameDevelopmentHerexamen/Implementation/Component/MouseInputComponent.cs
@@ -0,0 +1,57 @@
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Object.Component;
+using GameDevelopmentHerexamen.Framework.Utility;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Implementation.Component {
+    public class MouseInputComponent : IComponent {
+        public int Order { get; set; } = 0;
+
+        public delegate void MouseInputEventHandler(bool isClicked, bool isHovering);
+        public event MouseInputEventHandler InputEvent;
+        private (bool isClicked, bool isHovering) previousState;
+
+        public MouseInputComponent(
+                Action<bool, bool> anyHandler = null,
+                Action<bool> hoverHandler = null,
+                Action clickHandler = null) {
+            if (anyHandler != null) {
+                InputEvent += anyHandler.Invoke;
+            }
+
+            if (hoverHandler != null) {
+                InputEvent += (_, isHovering) => {
+                    if (previousState.isHovering != isHovering) {
+                        hoverHandler.Invoke(isHovering);
+                    }
+                };
+            }
+
+            if (clickHandler != null) {
+                InputEvent += (isClicked, isHovering) => {
+                    if (previousState.isClicked && !isClicked && isHovering) {
+                        clickHandler.Invoke();
+                    }
+                };
+            }
+        }
+
+        public void HandleInput(GameObject owner, InputState inputState) {
+            Rectangle mouseBounds = new Rectangle(inputState.MouseState.Position, Point.Zero);
+
+            owner.Bounds.Intersects(ref mouseBounds, out bool isHovering);
+            bool isClicked = isHovering && inputState.IsLeftClickPressed();
+
+            if (previousState.isClicked != isClicked || previousState.isHovering != isHovering) {
+                InputEvent.Invoke(isClicked, isHovering);
+            }
+
+            previousState = (isClicked, isHovering);
+        }
+    }
+}
