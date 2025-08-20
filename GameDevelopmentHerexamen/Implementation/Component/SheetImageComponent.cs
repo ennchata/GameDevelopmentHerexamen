d74307eff85146e5226ceae385ae12720e86be6f
s150673 <thibo.maes@student.ap.be>
2025-08-21 00:13:39 +0200

    Add image components

---
+++ GameDevelopmentHerexamen/Implementation/Component/SheetImageComponent.cs
@@ -0,0 +1,25 @@
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Object.Component;
+using GameDevelopmentHerexamen.Framework.Utility;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Implementation.Component {
+    public class SheetImageComponent : IComponent {
+        public string AssetReference { get; set; }
+        public Rectangle[] SourceRectangles { get; set; }
+        public int CurrentRectangle { get; set; }
+
+        public int Order { get; set; } = 0;
+
+        public void Draw(GameObject owner, SpriteBatch spriteBatch) {
+            Texture2D texture = AssetManager.Instance.Load<Texture2D>(AssetReference);
+            spriteBatch.Draw(texture, owner.Bounds, SourceRectangles[CurrentRectangle], Color.White);
+        }
+    }
+}
