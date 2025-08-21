0a4aa8ac931f50b3c8776b26d2a67f4afe285713
s150673 <thibo.maes@student.ap.be>
2025-08-21 21:51:55 +0200

    Add simple background to singleplayer scene

--- GameDevelopmentHerexamen/Implementation/Scene/Gameplay/SingleplayerScene.cs
+++ GameDevelopmentHerexamen/Implementation/Scene/Gameplay/SingleplayerScene.cs
@@ -1,5 +1,7 @@
-﻿using GameDevelopmentHerexamen.Framework.Scene;
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Scene;
 using GameDevelopmentHerexamen.Framework.Utility;
+using GameDevelopmentHerexamen.Implementation.Component;
 using GameDevelopmentHerexamen.Implementation.Object.Gameplay;
 using Microsoft.Xna.Framework;
 using System;
@@ -12,7 +14,14 @@ namespace GameDevelopmentHerexamen.Implementation.Scene.Gameplay {
     public class SingleplayerScene : GameScene {
         public SingleplayerScene() : base([
             new Player(new UDim2(0.5f, 0, 0.5f, 0)) {
-                ZIndex = -100
+                ZIndex = 100
+            },
+            new GameObject() {
+                Components = [
+                    new ImageComponent() {
+                        AssetReference = "images/bg"
+                    }
+                ]
             },
             new Ground() {
                 Position = UDim2.BottomCenter,
