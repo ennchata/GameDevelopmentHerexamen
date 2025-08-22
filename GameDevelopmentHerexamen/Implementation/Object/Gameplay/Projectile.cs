762161a31a55f3523cb4831f0855e7a9671547e3
s150673 <thibo.maes@student.ap.be>
2025-08-22 15:27:21 +0200

    Update movement step delegate to return UDim2 and take delta time parameter; add collider component

---
+++ GameDevelopmentHerexamen/Implementation/Object/Gameplay/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using GameDevelopmentHerexamen.Framework.Object;
+using GameDevelopmentHerexamen.Framework.Object.Component;
+using GameDevelopmentHerexamen.Framework.Utility;
+using GameDevelopmentHerexamen.Implementation.Component;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopmentHerexamen.Implementation.Object.Gameplay {
+    public class Projectile : GameObject {
+        private Player? playerReference;
+        private Func<Projectile, float, Player?, UDim2> movementStepFunction;
+
+        public Projectile(SheetImageComponent sheetImageComponent, Func<Projectile, float, Player?, UDim2> movementStepFunction, Player? playerReference = null): base() {
+            AddComponent(sheetImageComponent);
+            AddComponent(new ColliderComponent(collisionEnterHandler: (other) => {
+                if (other is Player) {
+                    // game over...
+                }
+            }));
+
+            this.playerReference = playerReference;
+            this.movementStepFunction = movementStepFunction;
+        }
+
+        public override void Update(GameTime gameTime) {
+            Position += movementStepFunction.Invoke(this, gameTime.ElapsedGameTime.Milliseconds / 1000f, playerReference);
+
+            base.Update(gameTime);
+        }
+    }
+}
